c5a9ce8dd11575a0cf2311dd70036c5770c369e9
Oskar Eichler <oskar@songstats.com>
2026-08-30 14:38:12 +0700

    Add serialized execution block coverage

--- spec/concurrent/executor/serialized_execution_spec.rb
+++ spec/concurrent/executor/serialized_execution_spec.rb
@@ -4,6 +4,15 @@ require_relative 'executor_service_shared'
 
 module Concurrent
 
+  RSpec.describe SerializedExecution do
+
+    it 'raises an exception when no block is given' do
+      expect {
+        subject.post(ImmediateExecutor.new)
+      }.to raise_error(ArgumentError, 'no block given')
+    end
+  end
+
   RSpec.describe SerializedExecutionDelegator do
 
     subject { SerializedExecutionDelegator.new(ImmediateExecutor.new) }
